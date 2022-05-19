e2a836ed510ccb7c76d63ab77b729143728f3338
Steven Johnson <sakurainds@gmail.com>
2022-05-19 15:49:24 +0700

    rename template to test app

--- src/main.rs
+++ src/main.rs
@@ -9,8 +9,8 @@ fn main() {
 
     let native_options = eframe::NativeOptions::default();
     eframe::run_native(
-        "eframe template",
+        "EGUI Test App",
         native_options,
-        Box::new(|cc| Box::new(eframe_template::TemplateApp::new(cc))),
+        Box::new(|cc| Box::new(egui_test_app::TestApp::new(cc))),
     );
 }
